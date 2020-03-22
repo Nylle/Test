4161d68ec8130d62eee7216bc5955b1106411301
Jan <Nylle@users.noreply.github.com>
2020-03-23 00:08:50 +0100

    Turn javadoc into kotlin-syntax, add dokka-maven-plugin

--- src/main/kotlin/com/github/nylle/test/Fixture.kt
+++ src/main/kotlin/com/github/nylle/test/Fixture.kt
@@ -12,7 +12,7 @@ class Fixture(configuration: Configuration = Configuration()) {
      * Creates a new object of the specified type, using a random constructor if available
      *
      * @param <T> the type of the object to be created
-     * @return a new object of the specified type {@code T}
+     * @return a new object of the specified type `T`
      */
     inline fun <reified T> construct(): T = javaFixture.construct(object : SpecimenType<T>(){})
 
@@ -20,30 +20,30 @@ class Fixture(configuration: Configuration = Configuration()) {
      * Creates a new object of the specified type, recursively populated with random values
      *
      * @param <T> the type of the object to be created
-     * @return a new object of the specified type {@code T}
+     * @return a new object of the specified type `T`
      */
     inline fun <reified T> create(): T = javaFixture.create(object : SpecimenType<T>(){})
 
     /**
-     * Creates a {@code Sequence} of objects of the specified type, recursively populated with random values
+     * Creates a [Sequence] of objects of the specified type, recursively populated with random values
      *
      * @param <T> the type of the objects to be created
-     * @return a {@code Sequence} of objects of the specified type {@code T}
+     * @return a [Sequence] of objects of the specified type `T`
      */
     inline fun <reified T> createMany(): Sequence<T> = javaFixture.createMany(object : SpecimenType<T>(){}).asSequence()
 
     /**
-     * Creates a {@code Sequence} of objects of the specified type, recursively populated with random values with the specified size
+     * Creates a [Sequence] of objects of the specified type, recursively populated with random values with the specified size
      *
-     * @param size the size of the {@code Sequence} to be created
+     * @param size the size of the [Sequence] to be created
      * @param <T> the type of the objects to be created
-     * @return a {@code Sequence} of objects of the specified type {@code T}
+     * @return a [Sequence] of objects of the specified type `T`
      */
     inline fun <reified T> createMany(size: Int): Sequence<T> = javaFixture.build(object : SpecimenType<T>(){}).createMany(size).asSequence()
 
     /**
-     * Adds objects of the specified type, recursively populated with random values, to the specified {@code Collection<T>}
-     * The number of objects created is specified in the {@code Configuration} under {@code streamSize} (default: 3)
+     * Adds objects of the specified type, recursively populated with random values, to the specified [Collection]
+     * The number of objects created is specified in the [Configuration] under `streamSize` (default: 3)
      *
      * @param result the collection the created objects should be added to
      * @param <T> the type of the collection and the objects to be added
@@ -51,7 +51,7 @@ class Fixture(configuration: Configuration = Configuration()) {
     inline fun <reified T> addManyTo(result: Collection<T>) = javaFixture.addManyTo(result, object : SpecimenType<T>(){})
 
     /**
-     * Creates a {@code SpecimenBuilder<T>} to customise the object of type {@code T} to be created
+     * Creates a [SpecimenBuilder] to customise the object of type `T` to be created
      *
      * @param <T> the type of the object to be created
      * @return a builder for further customisation
